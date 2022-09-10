e7d12b46070ad59463b9b67cf2c0da130e136787
Manuel Baez Diaz <baezdiazm@yahoo.com>
2022-09-09 23:29:23 -0400

    Adds a title and image to each individual listing

--- src/Apartment.js
+++ src/Apartment.js
@@ -3,16 +3,20 @@ import { useHistory, useRouteMatch, useLocation } from "react-router-dom"
 
 function Apartment({data}) {
     const history = useHistory()
-    const index = useLocation().pathname.split("")[11]-1
+    const match = useRouteMatch()
+    const location = useLocation().pathname.split("")[11]-1
 
     function back() {
         history.push("/")
     }
     
-    console.log(index)
+    console.log(data[location].image)
     return (
         <div>
             <button onClick={back}>Back</button>
+            <h1 className="listing-name">{data[location].name}</h1>
+            <img className="listing-image"src={data[location].image}/>
+            <h3>Some description will be here soon...</h3>
         </div>
     )
 }
