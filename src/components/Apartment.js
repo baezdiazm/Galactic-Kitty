a108e95a5d41b27f0fc4a10ef1aaff7483b2c244
Manuel Baez Diaz <baezdiazm@yahoo.com>
2022-09-10 21:02:17 -0400

    Adds description and styling to description

--- src/components/Apartment.js
+++ src/components/Apartment.js
@@ -14,8 +14,10 @@ function Apartment({data}) {
         <div>
             <button onClick={back}>Back</button>
             <h1 className="listing-name">{data[location].name}</h1>
+            <h2 className="listing-address">{data[location].address}</h2>
             <img className="listing-image"src={data[location].image} alt={data[location.name]}/>
-            <h3>Some description will be here soon...</h3>
+            <h1 className="description-h1">Description</h1>
+            <h3 className="description">{data[location].description}</h3>
         </div>
     )
 }
